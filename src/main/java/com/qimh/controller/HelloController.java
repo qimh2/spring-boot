852c083e60889f0e93c272e85e9a1901c41c888c
qimh2 <2237870348@qq.com>
2020-10-21 15:34:27 +0800

    添加日志切面

--- src/main/java/com/qimh/controller/HelloController.java
+++ src/main/java/com/qimh/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.qimh.controller;
 import com.qimh.aspect.MethodLog;
 import com.qimh.service.IService;
 import com.qimh.vo.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 public class HelloController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @Autowired
     private IService service;
 
     @RequestMapping("hello")
     @MethodLog(key = "cde",argsWhith = true)
     public String hello(@RequestBody Person person){
+        System.out.println("LOGGER:" + LOGGER);
+        LOGGER.info("hello:{}","world");
         return "hello world";
     }
 
