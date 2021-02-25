36d37133f6998064f2e690ba52eeb625af8c5eff
qimh2 <2237870348@qq.com>
2021-02-25 13:58:03 +0800

    集合交集

---
+++ src/test/java/com/qimh/springdemo/collection/Collection.java
@@ -0,0 +1,28 @@
+package com.qimh.springdemo.collection;
+
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
+
+public class Collection {
+
+    public static void main(String[] args) {
+        ArrayList list1 = Lists.newArrayList();
+        list1.add("aa");
+        list1.add("bb");
+        list1.add("cc");
+
+        ArrayList list2 = Lists.newArrayList();
+        list2.add("bb");
+        list2.add("cc");
+        list2.add("dd");
+        list2.add("ff");
+
+        // list1 与 list2 存在相同元素，list1集合只保留list2中存在的元素
+        //me---list1 与 list2 的交际，且把交集放入lis1中
+        list1.retainAll(list2);
+
+        System.out.println(list1);
+        System.out.println(list2);
+    }
+
+}
