3dcad22efeb898e5e82ecd155a31ebdae2c12227
qimh2 <2237870348@qq.com>
2021-04-02 17:35:44 +0800

    注解继承

--- src/main/java/com/qimh/service/Impl/ServiceImpl.java
+++ src/main/java/com/qimh/service/Impl/ServiceImpl.java
@@ -3,13 +3,17 @@ package com.qimh.service.Impl;
 import com.qimh.aspect.MethodLog;
 import com.qimh.service.IService;
 import com.qimh.vo.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceImpl implements IService {
+    Logger LOGGER = LoggerFactory.getLogger(ServiceImpl.class);
     @Override
     @MethodLog
     public Person getPerson(Person person) {
+        LOGGER.info("=====");
         Person p = new Person();
         p.setAge(20);
         p.setName("张三");
